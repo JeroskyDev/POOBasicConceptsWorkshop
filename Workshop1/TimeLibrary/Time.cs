588953eba41d7132fbf01b1ce43e3615a4a45a87
JERONIMORAMIREZRESTREPO <jeronimoramirez1159288@correo.itm.edu.co>
2026-02-25 02:05:56 -0500

    Just IsOtherDay function filling remaining >:D

--- Workshop1/TimeLibrary/Time.cs
+++ Workshop1/TimeLibrary/Time.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
 
 namespace TimeLibrary
 {
@@ -13,10 +14,29 @@ namespace TimeLibrary
         //constructors
         public Time()
         {
-            _hour = 0;
-            _minute = 0;
-            _second = 0;
-            _millisecond = 0;
+            Hour = 0;
+            Minute = 0;
+            Second = 0;
+            Millisecond = 0;
+        }
+        
+        //overloads
+        public Time(int hour)
+        {
+            Hour = hour;
+        }
+
+        public Time (int hour, int minute)
+        {
+            Hour = hour;
+            Minute = minute;
+        }
+
+        public Time (int hour, int minute, int second)
+        {
+            Hour = hour;
+            Minute = minute;
+            Second = second;
         }
 
         public Time(int hour, int minute, int second, int millisecond)
@@ -55,39 +75,112 @@ namespace TimeLibrary
         //methods
 
         //public methods
-        public override string ToString() => $"{Hour:00}:{Minute:00}:{Second:00}:{Millisecond:000} tt"; //replace tt with AM or PM
-
-        public object Add(object Time)
+        public override string ToString()
         {
-            return Time;
+            string tt = Hour < 12 ? "AM" : "PM";
+            int hourToDisplay = Hour; //temporal local variable to not change the whole Hour property
+            if (hourToDisplay == 0)
+            {
+                hourToDisplay = 12;
+                tt = "AM";
+            }
+            else if (hourToDisplay >= 1 && hourToDisplay <= 11)
+            {
+                tt = "AM";
+            }
+            else if (hourToDisplay >= 13 && hourToDisplay <= 23)
+            {
+                hourToDisplay -= 12;
+                tt = "PM";
+            }
+            else
+            {
+                tt = "PM";
+            }
+            return $"{hourToDisplay:00}:{Minute:00}:{Second:00}.{Millisecond:000} {tt}"; 
+            
         }
 
-        public bool IsOtherDay(object Time)
+        public object Add(Time time)
         {
+            //do summation component by component and with carry
+            int milliseconds = Millisecond + time.Millisecond;
+            int seconds = 0;
+            int minutes = 0;
+            int hours = 0;
+            int carrySeconds = milliseconds / 1000; //seconds to add if milliseconds were more than 999
+            
+            seconds = Second + time.Second + carrySeconds;
+            int carryMinutes = seconds / 60;
+            milliseconds = milliseconds % 1000; //milliseconds remaining if higher than 999 (it gets turned into seconds)
+
+            minutes = Minute + time.Minute + carryMinutes;
+            int carryHours = minutes / 60;
+            seconds = seconds % 60;
+
+            hours = Hour + time.Hour + carryHours;
+            minutes = minutes % 60;
+
+            if (hours > 23)
+            {
+                hours = hours % 24;
+            }
+
+            return new Time(hours, minutes, seconds, milliseconds);
+        }
+
+        public bool IsOtherDay(Time time)
+        {
+            if (time == null) return false;
+
             return false;
         }
 
-        public int ToMinutes(object Time, int minute)
+        public int ToMinutes()
         {
-            return 0;
+            int minutesToDisplay = 0;
+            ValidMinute(Minute);
+            
+            //do calculations 
+            minutesToDisplay = ToMilliseconds() / 60000;
+
+            return minutesToDisplay;
         }
 
-        public int ToSeconds(object Time, int second)
+        public int ToSeconds()
         {
-            return 0;
+            int secondsToDisplay = 0;
+            ValidSecond(Second);
+            
+            //do calculations 
+            secondsToDisplay = ToMilliseconds() / 1000;
+
+            return secondsToDisplay;
         }
 
-        public int ToMilliseconds(object Time, int millisecond)
+        public int ToMilliseconds()
         {
-            return 0;
+            int millisecondsToDisplay = 0;
+
+            //constants for conversion
+            int hourOnMilliseconds = (60 * 60 * 1000);
+            int minuteOnMilliseconds = (60 * 1000);
+            int secondOnMilliseconds = 1000;
+
+            ValidMillisecond(Millisecond);
+            
+            //do calculations
+            millisecondsToDisplay = (Hour * hourOnMilliseconds) + (Minute * minuteOnMilliseconds) + (Second * secondOnMilliseconds) + Millisecond;
+
+            return millisecondsToDisplay;
         }
 
         //private methods
         private int ValidHour(int hour)
         {
-            if (hour < 0 || hour > 24)
+            if (hour < 0 || hour > 23)
             {
-                throw new ArgumentOutOfRangeException(nameof(hour), "Hours MUST be GREATER than 0 or LESS than 24.");
+                throw new ArgumentException($"The hour: {hour}, is not valid.");
             }
             return hour;
         }
@@ -96,7 +189,7 @@ namespace TimeLibrary
         {
             if (minute < 0 || minute > 59)
             {
-                throw new ArgumentOutOfRangeException(nameof(minute), "Minutes MUST be GREATER than 0 or LESS than 59.");
+                throw new ArgumentException($"The minute: {minute}, is not valid.");
             }
             return minute;
         }
@@ -105,7 +198,7 @@ namespace TimeLibrary
         {
             if (second < 0 || second > 59)
             {
-                throw new ArgumentOutOfRangeException(nameof(second), "Seconds MUST be GREATER than 0 or LESS than 59.");
+                throw new ArgumentException($"The second: {second}, is not valid.");
             }
             return second;
         }
@@ -114,7 +207,7 @@ namespace TimeLibrary
         {
             if (millisecond < 0 || millisecond > 999)
             {
-                throw new ArgumentOutOfRangeException(nameof(millisecond), "Milliseconds MUST be GREATER than 0 or LESS than 999.");
+                throw new ArgumentException($"The millisecond: {millisecond}, is not valid.");
             }
             return millisecond;
         }
